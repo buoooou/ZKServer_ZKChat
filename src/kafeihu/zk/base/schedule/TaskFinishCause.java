81ef3bdcae8119d2734e4a987975976f934edf6a
zhangkuo <769329489@qq.com>
2017-06-03 22:07:22 +0800

    Task

--- src/kafeihu/zk/base/schedule/TaskFinishCause.java
+++ src/kafeihu/zk/base/schedule/TaskFinishCause.java
@@ -1,7 +1,20 @@
 package kafeihu.zk.base.schedule;
 
 /**
+ * 任务结束原因
  * Created by zhangkuo on 2017/6/1.
  */
-public class TaskFinishCause {
+public enum TaskFinishCause {
+    /**
+     * 因任务过期而结束
+     */
+    Expired,
+    /**
+     * 因达到重复执行次数而结束
+     */
+    ReachRepeatCount,
+    /**
+     * 因主动撤销任务执行而结束
+     */
+    Cancelled;
 }
