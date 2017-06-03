355602b5820090a78890651ce312f0d5b507bb81
zhangkuo <769329489@qq.com>
2017-06-03 23:47:07 +0800

    定时任务基础类

---
+++ src/kafeihu/zk/bserver/schedule/ModuleTask.java
@@ -0,0 +1,58 @@
+package kafeihu.zk.bserver.schedule;
+
+import kafeihu.zk.base.logging.Logger;
+import kafeihu.zk.base.schedule.Task;
+import kafeihu.zk.bserver.context.ModuleContext;
+import kafeihu.zk.bserver.manager.ContextManager;
+import kafeihu.zk.bserver.manager.LoggerManager;
+
+/**
+ * Created by zhangkuo on 2017/6/3.
+ */
+public abstract class ModuleTask extends Task{
+    private final String m_moduleName;
+
+    public ModuleTask(String name, String moduleName)
+    {
+        super(name);
+        m_moduleName = moduleName;
+    }
+
+    public String getModuleName()
+    {
+        return m_moduleName;
+    }
+    /**
+     * 获取模块日志处理类
+     *
+     * @return
+     */
+    public Logger getModuleLogger()
+    {
+        return LoggerManager.getModuleLogger(m_moduleName);
+    }
+
+    /**
+     * 获取模块上下文
+     *
+     * @return
+     */
+    public ModuleContext getModuleContext()
+    {
+        try
+        {
+            return ContextManager.getModuleContext(m_moduleName);
+        }
+        catch (Exception exp)
+        {
+            // should not reach here
+            throw new RuntimeException(exp.getMessage(), exp);
+        }
+    }
+    @Override
+    public String toString()
+    {
+        return "ModuleTask [name=" + getName() + " , moduleName=" + m_moduleName + "]";
+    }
+
+}
