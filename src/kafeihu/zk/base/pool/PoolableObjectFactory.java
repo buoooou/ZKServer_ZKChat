c7ca1ba9f9f8c8a9fc8030ad122bbc4427865325
zhangkuo <769329489@qq.com>
2017-06-17 17:15:18 +0800

    objectPool

--- src/kafeihu/zk/base/pool/PoolableObjectFactory.java
+++ src/kafeihu/zk/base/pool/PoolableObjectFactory.java
@@ -3,5 +3,63 @@ package kafeihu.zk.base.pool;
 /**
  * Created by zhangkuo on 2017/6/15.
  */
-public class PoolableObjectFactory {
+public interface PoolableObjectFactory {
+    /**
+     * 构造对象
+     *
+     * @return
+     * @throws Exception
+     *             构造对象失败
+     */
+    Object makeObject() throws Exception;
+
+    /**
+     * 根据指定的资源对象配置构造对象
+     *
+     * @param poolObjConfig
+     *            指定的资源对象配置
+     * @return
+     * @throws Exception
+     */
+    Object makeObject(PoolableObjectConfig poolObjConfig) throws Exception;
+
+    /**
+     * 销毁对象
+     *
+     * @param obj
+     * @throws Exception
+     */
+    void destroyObject(Object obj) throws Exception;
+
+    /**
+     * 判断对象是否有效。如果对象已无效，则调用destroyObject方法销毁对象
+     *
+     * @param obj
+     * @return false 如果对象已无效
+     */
+    boolean validateObject(Object obj);
+
+    /**
+     * 激活对象，使对象处于可用状态。
+     *
+     * @param obj
+     * @throws Exception
+     *             激活对象失败，对象已不可用
+     */
+    void activateObject(Object obj) throws Exception;
+
+    /**
+     * 钝化对象，使对象处于挂起/休眠状态。
+     *
+     * @param obj
+     * @throws Exception
+     */
+    void passivateObject(Object obj) throws Exception;
+
+    /**
+     * 连接池目标信息
+     *
+     * @return
+     */
+    String targetInfo();
 }
