ad3b73e1bde1140708f1628ea9c0273b2f5aeb28
zhangkuo <769329489@qq.com>
2017-06-18 13:04:41 +0800

    DBHolder

---
+++ src/kafeihu/zk/bserver/proc/database/DBConnPoolHolder.java
@@ -0,0 +1,67 @@
+package kafeihu.zk.bserver.proc.database;
+
+import kafeihu.zk.bserver.manager.DBConnectionPoolManager;
+import kafeihu.zk.database.pool.DBConnectionPool;
+
+/**
+ * 数据库连接池实例持有者：应用通过该类获取特定的数据库连接池
+ *
+ *
+ * Created by zhangkuo on 2017/6/18.
+ */
+public class DBConnPoolHolder {
+    private DBConnPoolHolder()
+    {
+    }
+
+    /**
+     * 数据库连接池实例。目标数据库为网上支付数据库NETPAY
+     */
+    public static DBConnectionPool NETPAY;
+    /**
+     * 数据库连接池实例。目标数据库为专业版数据库PB
+     */
+    // public static DBConnectionPool PB;
+
+    static
+    {
+        try
+        {
+            System.out.print("Initializing DBConnPoolHolder...... ");
+            initialize();
+            System.out.println("OK!");
+        }
+        catch (Exception exp)
+        {
+            throw new ExceptionInInitializerError(DBConnPoolHolder.class.getName() + ".initialize(). " + exp);
+        }
+    }
+
+    /**
+     * 数据库实例初始化
+     *
+     * @throws Exception
+     */
+    private static void initialize() throws Exception
+    {
+        NETPAY = DBConnectionPoolManager.getSysDBConnectionPool("zkchat");
+    }
+
+    /**
+     * 通过数据库ID获取数据库连接池
+     * @param dbID
+     * @return
+     * @throws NetpayException
+     */
+    public static DBConnectionPool getDBConnPool(String dbID) throws NetpayException
+    {
+        try
+        {
+            return DBConnectionPoolManager.getSysDBConnectionPool(dbID);
+        }
+        catch (Exception exp)
+        {
+            throw new NetpayException(NetpayErrorCode.DBProc_Exception, exp.getMessage());
+        }
+    }
+}
