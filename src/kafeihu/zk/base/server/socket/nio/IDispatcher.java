1cd3475bd5580736df515321130b0a38855b54b7
zhangkuo <769329489@qq.com>
2017-07-10 21:55:35 +0800

    dispatcher

--- src/kafeihu/zk/base/server/socket/nio/IDispatcher.java
+++ src/kafeihu/zk/base/server/socket/nio/IDispatcher.java
@@ -1,7 +1,46 @@
 package kafeihu.zk.base.server.socket.nio;
 
+import kafeihu.zk.base.server.socket.ISocketServer;
+
+import java.io.IOException;
+import java.nio.channels.SelectableChannel;
+
 /**
  * Created by zhangkuo on 2017/7/9.
  */
-public class IDispatcher {
+public interface IDispatcher {
+
+    /**
+     *
+     * @throws IOException
+     */
+    void dispatch() throws IOException;
+
+    /**
+     *
+     * @return
+     */
+    boolean isDispatching();
+    /**
+     * ֹͣ���ȷַ�����
+     */
+    void shutdown();
+    /**
+     *
+     * @param channel
+     * @param handler
+     * @param readQueue
+     * @return
+     * @throws IOException
+     */
+    IChannelFacade registerChannel(ISocketServer socketSrc, SelectableChannel channel, INioSocketRequestHandler handler, IReadQueue readQueue, IWriteQueue writeQueue);
+    /**
+     *
+     * @param adapter
+     */
+    void unRegisterChannel(IChannelFacade adapter);
+    /**
+     *
+     */
+    int getRegisteredChannelNum();
 }
