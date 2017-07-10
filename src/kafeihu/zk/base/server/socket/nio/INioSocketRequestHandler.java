9ea24bc8eded77376c9c8a73110bfd795c89f254
zhangkuo <769329489@qq.com>
2017-07-10 21:56:16 +0800

    handler

---
+++ src/kafeihu/zk/base/server/socket/nio/INioSocketRequestHandler.java
@@ -0,0 +1,7 @@
+package kafeihu.zk.base.server.socket.nio;
+
+/**
+ * Created by zhangkuo on 2017/7/10.
+ */
+public class INioSocketRequestHandler {
+}
--- src/kafeihu/zk/base/server/socket/nio/INioSocketRequestHandlerFactory.java
+++ src/kafeihu/zk/base/server/socket/nio/INioSocketRequestHandlerFactory.java
@@ -1,7 +1,29 @@
 package kafeihu.zk.base.server.socket.nio;
 
 /**
+ * 请求处理器工厂类接口
+ *
  * Created by zhangkuo on 2017/7/9.
  */
-public class INioSocketRequestHandlerFactory {
+public interface INioSocketRequestHandlerFactory {
+    /**
+     * 构造新的请求处理器实例
+     * @return
+     */
+    INioSocketRequestHandler newRequestHandler();
+
+    /**
+     * 构造读缓冲队列
+     *
+     * @return
+     */
+    IReadQueue createReadQueue();
+
+
+    /**
+     * 构造写缓冲队列。子类可重写该方法构造特定的写队列
+     *
+     * @return
+     */
+    IWriteQueue createWriteQueue();
 }
