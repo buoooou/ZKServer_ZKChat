c15f47b1f2c7446c0327c7b46a0bd7b2d3ff4adc
zhangkuo <769329489@qq.com>
2017-07-11 21:03:07 +0800

    handler

--- src/kafeihu/zk/base/server/socket/nio/INioSocketRequestHandler.java
+++ src/kafeihu/zk/base/server/socket/nio/INioSocketRequestHandler.java
@@ -1,7 +1,44 @@
 package kafeihu.zk.base.server.socket.nio;
 
+import java.nio.ByteBuffer;
+
 /**
  * Created by zhangkuo on 2017/7/10.
  */
-public class INioSocketRequestHandler {
+public interface INioSocketRequestHandler {
+    /**
+     *
+     * @param channelFacade
+     * @return
+     */
+    ByteBuffer nextRequestData(IChannelFacade channelFacade);
+
+    /**
+     *
+     * @param requestBuf
+     * @param channelFacade
+     * @return
+     */
+    boolean handleRequestData(ByteBuffer requestBuf, IChannelFacade channelFacade);
+
+    /**
+     *
+     * @param channelFacade
+     */
+    void starting(IChannelFacade channelFacade);
+    /**
+     *
+     * @param channelFacade
+     */
+    void started(IChannelFacade channelFacade);
+    /**
+     *
+     * @param channelFacade
+     */
+    void stopping(IChannelFacade channelFacade);
+    /**
+     *
+     * @param channelFacade
+     */
+    void stopped(IChannelFacade channelFacade);
 }
