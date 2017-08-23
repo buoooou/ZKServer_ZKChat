fbecff2fe39452127c7597ce4050b532982d53dd
zhangkuo <769329489@qq.com>
2017-08-23 20:52:45 +0800

    log4j日志

--- UniBServer/java/kafeihu/zk/bserver/manager/Log4JManager.java
+++ UniBServer/java/kafeihu/zk/bserver/manager/Log4JManager.java
@@ -1,5 +1,6 @@
 package kafeihu.zk.bserver.manager;
 
+import kafeihu.zk.base.logging.log4j.SysLogger;
 import kafeihu.zk.base.util.ResourceUtil;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
@@ -9,6 +10,9 @@ import org.apache.log4j.PropertyConfigurator;
  */
 public final class Log4JManager {
 
+    private static Logger m_monitorLogger = Logger.getLogger("MONITORLOG");
+    private static Logger m_zkchatLogger = Logger.getLogger("ZKCHATLOG");
+    private static SysLogger m_sysLogger = new SysLogger();
 
     static
     {
@@ -37,51 +41,27 @@ public final class Log4JManager {
      *
      * @return
      */
-    public static LoggerUtil getSysLogger() {
+    public static SysLogger getSysLogger() {
 
-        return new LoggerUtil();
+        return m_sysLogger;
     }
     /**
      * 获取指定模块关联的日志处理器
      *
-     * @param moduleName
      * @return
      */
-    public static Logger getModuleLogger(String moduleName)
+    public static Logger getZKChatLogger()
     {
-       return Logger.getLogger(moduleName);
+       return m_zkchatLogger;
     }
-
-    public static void main(String[] args) {
-        //m_debugLogger.debug("dsd");getSysLogger()
-        getSysLogger().info("dafafasddff");
-    }
-}
-class LoggerUtil{
-
     /**
-     * 系统日志处理器实例
+     * 获取指定模块关联的日志处理器
+     *
+     * @return
      */
-    private static Logger m_debugLogger = Logger.getLogger("DEBUGLOG");
-    private static Logger m_infoLogger = Logger.getLogger("INFOLOG");
-    private static Logger m_warnLogger = Logger.getLogger("WARNLOG");
-    private static Logger m_errorLogger = Logger.getLogger("ERRORLOG");
-
-    public static void info(String log){
-        m_infoLogger.info(log);
-    };
-
-    public static void warn(String log){
-        m_warnLogger.warn(log);
-    };
-
-
-    public static void error(String log){
-        m_errorLogger.error(log);
-    };
-
-    public static void debug(String log){
-        m_debugLogger.debug(log);
-    };
-
+    public static Logger getMonitorLogger()
+    {
+        return m_monitorLogger;
+    }
 }
+
