cb5fdd0f58010c4189c2a4bc69ba1c778bbcd70f
zhangkuo <769329489@qq.com>
2017-08-26 13:07:42 +0800

    新加slf4j

--- java/kafeihu/zk/bserver/test/Slf4jUtil.java
+++ java/kafeihu/zk/bserver/test/Slf4jUtil.java
@@ -1,7 +1,7 @@
 package kafeihu.zk.bserver.test;
 
 import kafeihu.zk.base.util.ResourceUtil;
-import org.apache.log4j.PropertyConfigurator;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,11 +9,11 @@ import org.slf4j.LoggerFactory;
  * Created by zhangkuo on 2017/8/26.
  */
 public final class Slf4jUtil {
-    public static void main(String[] args) {
-        String path = ResourceUtil.getSysDataPath();
-        PropertyConfigurator.configure(path + "log4j.properties");
-        Logger log = LoggerFactory.getLogger("console");
-        log.info("log.info(...)");
-    }
+//    public static void main(String[] args) {
+//        String path = ResourceUtil.getSysDataPath();
+//        PropertyConfigurator.configure(path + "log4j.properties");
+//        Logger log = LoggerFactory.getLogger("console");
+//        log.info("log.info(...)");
+//    }
 
 }
