d023e6f3f11396442c617eb432acbc682ca68587
zhangkuo <769329489@qq.com>
2017-11-08 20:13:01 +0800

    netty引导bootstrap

--- ZKServer/java/kafeihu/zk/bserver/netty/NettyServerBootstrap.java
+++ ZKServer/java/kafeihu/zk/bserver/netty/NettyServerBootstrap.java
@@ -7,10 +7,31 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import kafeihu.zk.base.server.IServer;
+import kafeihu.zk.bserver.manager.Slf4JManager;
+import org.slf4j.Logger;
 
 public class NettyServerBootstrap implements IServer {
 
-    static final int PORT = Integer.parseInt(System.getProperty("port", "1080"));
+    private final int PORT = Integer.parseInt(System.getProperty("port", "1080"));
+
+    private static final Logger m_logger = Slf4JManager.getSysLogger();
+    /**
+     * 服务器别名
+     */
+    private String m_alias;
+    /**
+     * 服务器启动时间
+     */
+    private long m_startUpTime = System.currentTimeMillis();
+
+    /**
+     * 服务器停止时间
+     */
+    private long m_stopTime = -1;
+
+    private EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+    private EventLoopGroup workerGroup = new NioEventLoopGroup();
+
 
     @Override
     public boolean isRunning() {
@@ -18,52 +39,54 @@ public class NettyServerBootstrap implements IServer {
     }
 
     @Override
-    public void onServerStarting() {
-
-    }
-
-    @Override
     public void start() throws Exception {
 
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
-        try {
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .handler(new LoggingHandler(LogLevel.INFO))
-                    .childHandler(new SocksServerInitializer());
-            b.bind(PORT).sync().channel().closeFuture().sync();
-        } finally {
-            bossGroup.shutdownGracefully();
-            workerGroup.shutdownGracefully();
-        }
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .childHandler(new SocksServerInitializer());
+        b.bind(PORT).sync().channel().closeFuture().sync();
+
+
+    }
+
+
+    @Override
+    public void onServerStarting()
+    {
+        m_startUpTime = System.currentTimeMillis();
+    }
+
+    @Override
+    public void onServerStarted()
+    {
+        m_logger.info(getClass().getName(), "started. " + toString());
 
     }
 
     @Override
-    public void onServerStarted() {
-
+    public void onServerStopped()
+    {
+        // TODO Auto-generated method stub
+        m_stopTime = System.currentTimeMillis();
+        m_logger.info(getClass().getName(), "stopped. " + toString());
     }
 
     @Override
     public void onServerStopping() {
-
     }
 
     @Override
     public void stop() throws Exception {
 
-    }
-
-    @Override
-    public void onServerStopped() {
-
+        bossGroup.shutdownGracefully();
+        workerGroup.shutdownGracefully();
     }
 
     @Override
     public String getAlias() {
-        return null;
+        return m_alias;
     }
 
     @Override
